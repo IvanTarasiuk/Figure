a4f92e26e01639c740974465660b474b26679f26
vanechka <tarasyuk.education@gmail.com>
2023-11-25 11:08:01 +0300

    commit

--- src/main/java/com/github/vanechka/Figure/classes/Cone.java
+++ src/main/java/com/github/vanechka/Figure/classes/Cone.java
@@ -4,46 +4,44 @@ import java.util.ArrayList;
 
 public class Cone extends Figure {
 
-    private ArrayList<Integer> Point;
+    private ArrayList<Integer> point;
     private int count;
 
     public Cone(ArrayList<Integer> Point, int count) {
         super(Point);
-        this.Point = Point;
+        this.point = point;
         this.count = count;
     }
 
     @Override
     public boolean isFigureValid() {
-        if (Point.size() == NINE && count == 3) {
-            int vecx1 = Point.get(INDEX_THREE) - Point.get(0);
-            int vecy1 = Point.get(INDEX_FOUR) - Point.get(1);
-            int vecz1 = Point.get(INDEX_FIVE) - Point.get(INDEX_TWO);
+        if (point.size() == NINE && count == 3) {
+            int vecx1 = point.get(INDEX_THREE) - point.get(0);
+            int vecy1 = point.get(INDEX_FOUR) - point.get(1);
+            int vecz1 = point.get(INDEX_FIVE) - point.get(INDEX_TWO);
 
-            int vecx2 = Point.get(INDEX_SIX) - Point.get(0);
-            int vecy2 = Point.get(INDEX_SEVEN) - Point.get(1);
-            int vecz2 = Point.get(INDEX_EIGHT) - Point.get(INDEX_TWO);
+            int vecx2 = point.get(INDEX_SIX) - point.get(0);
+            int vecy2 = point.get(INDEX_SEVEN) - point.get(1);
+            int vecz2 = point.get(INDEX_EIGHT) - point.get(INDEX_TWO);
 
-            if ((vecx1 * vecx2 + vecy1 * vecy2 + vecz1 * vecz2) / (Math.sqrt(Math.pow(vecx1, DEGREE) + Math.pow(vecy1, DEGREE) + Math.pow(vecy1, DEGREE) * Math.sqrt(Math.pow(vecx2, DEGREE) + Math.pow(vecy2, DEGREE) + Math.pow(vecy2, DEGREE) ))) == 0) {
-                return true;
-            }
+            return (vecx1 * vecx2 + vecy1 * vecy2 + vecz1 * vecz2) / (Math.sqrt(Math.pow(vecx1, DEGREE) + Math.pow(vecy1, DEGREE) + Math.pow(vecy1, DEGREE) * Math.sqrt(Math.pow(vecx2, DEGREE) + Math.pow(vecy2, DEGREE) + Math.pow(vecy2, DEGREE) ))) == 0;
         }
         return false;
     }
 
     @Override
     public void areaFigure() {
-        int vecx1 = Point.get(INDEX_THREE) - Point.get(0);
-        int vecy1 = Point.get(INDEX_FOUR) - Point.get(1);
-        int vecz1 = Point.get(INDEX_FIVE) - Point.get(INDEX_TWO);
+        int vecx1 = point.get(INDEX_THREE) - point.get(0);
+        int vecy1 = point.get(INDEX_FOUR) - point.get(1);
+        int vecz1 = point.get(INDEX_FIVE) - point.get(INDEX_TWO);
 
-        int vecx2 = Point.get(INDEX_SIX) - Point.get(0);
-        int vecy2 = Point.get(INDEX_SEVEN) - Point.get(1);
-        int vecz2 = Point.get(INDEX_EIGHT) - Point.get(INDEX_TWO);
+        int vecx2 = point.get(INDEX_SIX) - point.get(0);
+        int vecy2 = point.get(INDEX_SEVEN) - point.get(1);
+        int vecz2 = point.get(INDEX_EIGHT) - point.get(INDEX_TWO);
 
-        int vecx3 = Point.get(INDEX_THREE) - Point.get(INDEX_SIX);
-        int vecy3 = Point.get(INDEX_FOUR) - Point.get(INDEX_SEVEN);
-        int vecz3 = Point.get(INDEX_FIVE) - Point.get(INDEX_EIGHT);
+        int vecx3 = point.get(INDEX_THREE) - point.get(INDEX_SIX);
+        int vecy3 = point.get(INDEX_FOUR) - point.get(INDEX_SEVEN);
+        int vecz3 = point.get(INDEX_FIVE) - point.get(INDEX_EIGHT);
 
         double radius = Math.sqrt(Math.pow(vecx1, DEGREE) + Math.pow(vecy1, DEGREE) + Math.pow(vecz1, DEGREE));
         double h = Math.sqrt(Math.pow(vecx2, DEGREE) + Math.pow(vecy2, DEGREE) + Math.pow(vecz2, DEGREE));
